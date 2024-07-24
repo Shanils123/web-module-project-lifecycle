7d97ec5f11bb931e114e97948cd3464c1f83f374
Shanil Sanchez <shanils857@gmail.com>
2024-07-24 15:30:11 -0400

    completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
 import { error } from 'ajv/dist/vocabularies/applicator/dependencies'
 
 const URL = 'http://localhost:9000/api/todos'
@@ -9,6 +10,7 @@ export default class App extends React.Component {
     todos: [],
     error: '',
     todoNameInput: '',
+    displayCompleted: true,
   }
 
   onTodoNameInputChange = evt => {
@@ -56,6 +58,10 @@ export default class App extends React.Component {
       .catch(this.setAxiosResponseError)
   }
 
+  toggleDisplayCompleted = () => {
+    this.setState({ ...this.state, displayCompleted: !this.state.displayCompleted})
+  }
+
   componentDidMount() {
     //fetch all todos from server
     this.fetchAllTodos()
@@ -67,17 +73,23 @@ export default class App extends React.Component {
       <div id='todos'>
         <h2>Todos: </h2>
         {
-          this.state.todos.map(td => {
-            return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✓' : ''}</div>
-          })
+          this.state.todos.reduce((acc, td) => {
+            if(this.state.displayCompleted || !td.completed) return acc.concat(
+              <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✓' : ''}</div>
+            )
+            return acc
+          }, [])
+            //return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✓' : ''}</div>
         }
       </div>
-      <form onSubmit={this.onTodoFormSubmit} id='todoForm'>
-        <input value= {this.state.todoNameInput} onChange={this.onTodoNameInputChange} type='text' placeholder='Type todo'></input>
-        <input type='submit'></input>
-        <button>Clear Completed</button>
-      </form>
-    </div>
+      <Form
+      onTodoFormSubmit={this.onTodoFormSubmit}
+      todoNameInput={this.state.todoNameInput}
+      onTodoNameInputChange={this.onTodoNameInputChange}
+      toggleDisplayCompleted={this.toggleDisplayCompleted}
+      displayCompleted={this.state.displayCompleted}
+      />
+      </div>
     )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <>
+      <form onSubmit={this.props.onTodoFormSubmit} id='todoForm'>
+        <input 
+          value= {this.props.todoNameInput} 
+          onChange={this.props.onTodoNameInputChange} 
+          type='text' 
+          placeholder='Type todo'></input>
+        
+        <input type='submit'></input>
+      
+      </form>
+      <button 
+      onClick={this.props.toggleDisplayCompleted}>
+      { this.props.displayCompleted ? 'Hide' : 'Show' } Completed
+      </button>
+      </>
+    )
   }
 }
